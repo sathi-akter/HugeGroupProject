00e4be98ef2c3c48c6778c0a514b51f3aea99f74
AmmanBhat <83989549+AmmanBhat@users.noreply.github.com>
2021-07-14 15:14:25 -0400

    db working, add button not functional, working on that next

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/competitor";
-exports.RemoteURI = "mongodb + srv://admin:mypassword7890@database-server.mem50.mongodb.net/tournament_store?retryWrites=true&w=majority";
+exports.RemoteURI = "mongodb+srv://admin:mypassword7890@database-server.mem50.mongodb.net/tournament_store?retryWrites=true&w=majority";
 exports.HostName = "RemoteHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://localhost/competitor";
-export const RemoteURI = "mongodb + srv://admin:mypassword7890@database-server.mem50.mongodb.net/tournament_store?retryWrites=true&w=majority";
+export const RemoteURI = "mongodb+srv://admin:mypassword7890@database-server.mem50.mongodb.net/tournament_store?retryWrites=true&w=majority";
 export const HostName = "RemoteHost";
 export const Secret = "someSecret";
